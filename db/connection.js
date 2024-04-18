53371844bf0de9af2b299bec1866bb47eceaf5de
rmachaal <rahafam@hotmail.co.uk>
2024-04-18 10:57:46 +0100

    Sets up production environment.

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,18 @@
 const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || 'development' || 'production'
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set');
 }
 
